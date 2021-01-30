f367c226a11e8c6884b7df53aa8adff769d7d240
youhavetohearthis <76894186+youhavetohearthis@users.noreply.github.com>
2021-01-30 17:29:20 +0000

    Create 2021-01-30-wherethelightsarelow-tobyromeo.m

---
+++ _posts/2021-01-30-wherethelightsarelow-tobyromeo.m
@@ -0,0 +1,20 @@
+---
+layout: post
+date: 2021-01-30 17:28:30
+title: "Where The Lights Are Low"
+artist: "Toby Romeo, Felix Jaehn & FAULHABER"
+spotify: 
+    - https://open.spotify.com/track/0Cnb8XdGDMHAmIRBUJ
+applemusic: 
+    - https://music.apple.com/gb/album/where-the-lights-are-low/1546569190?i=1546569192
+---
+
+In the wake of the week where Sea Shanties met Tik Tok and produced a top three UK single (!), we present to you a huge Saturday night banger that flirts within the same realm, but without being anywhere near as.... shiver me timbers. Today **you have to hear** the new effort from Toby Romeo and friends!
+
+The reason we make the comparison is that the chorus has a definite nursery rhyme quality to it, showcasing a simple but effective earworm melody. The rest of the song offers much more complexity though, with stabs of strings decorating the bridge, deep bass punctuating throughout and plenty of synths. Though it does bring a childlike refrain to the party, it also knows it’s a party and remembers to bring a big dumping of cool too.  
+
+It’s a song about... shagging in the dark, basically. The age old tale of saying you aren’t going to go there this time.... and fully going there this time. ‘I can tell your body feel the same, feel the same, put our hands in places we don’t want them to know... come and meet me where the lights are low’. Somebody has the lockdown horn, ladies and gents.
+
+It’s a short little banger clocking in at just over two minutes, but still lodges the chorus in your head and finds enough time to throw around plenty of interesting melody ideas, for sure the kind of song you and your mates would bellow out once the dancefloor is a safe place again! 
+
+**For fans of:** Avicii, having very little self restraint, wanting to get back on a bloody dancefloor.
